1b16a0ce1d086432ccb14a3273e3060791a57aa3
Pim <30662877+PimMiii@users.noreply.github.com>
2025-01-28 12:20:36 +0100

    Create announcements.js

---
+++ announcements.js
@@ -0,0 +1,14 @@
+{
+  "appnews": {
+    "appid": 0,
+    "newsitems": [
+      {
+        "title": "Sideloaded 1",
+        "url": "https://google.com",
+        "is_external_url": true,
+        "author": "sillybusinessstudios",
+        "contents": "[img]{EXTERNAL_IMG_HOST}[/img]",
+      }
+    ]
+  }
+}
